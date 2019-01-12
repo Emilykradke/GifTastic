92bff9dcd4150fd49ffc83a8a226da8a24730f2e
Emilykradke <emilykradke@gmail.com>
2019-01-11 20:19:56 -0600

    buttonsarray

--- assets/JavaScript/giphy.js
+++ assets/JavaScript/giphy.js
@@ -1,4 +1,7 @@
+
+// array of animals that will be the buttons that start on the page 
 var animals = ["dog", "penguin", "goldfish", "reindeer", "cat", "killer whale", "panda bear", "wolf", "racoon", "mouse"];
+
 
 // function that takes each item in the array and makes a button for it
 var createButton = function() {
@@ -9,6 +12,7 @@ var createButton = function() {
     // loop through the array and make buttons for them
     for(var i = 0; i < animals.length; i++) {
         
+        // create the button div
         var butt = $("<button>");
         // add a class of animal-button to the buttons
         butt.addClass("animal-button");
@@ -18,6 +22,32 @@ var createButton = function() {
         butt.text(animals[i]);
         // put the buttons into the html in the buttons div
         $("#buttons").append(butt);
+
     }
 }
-createButton()
+
+// on click of the submit button, take the html form input and push it to the 'animals' array
+$(document.body).on("click", "#add-animal", function(){
+    event.preventDefault();
+    // Grab the input from the textbox
+    var newAnimal = $("#animal-input").val().trim();
+    // adding the animal input from the textbox to our "animals" array
+    animals.push(newAnimal);
+    // call the createButton function to process the new item that has been put into the animals array
+    console.log(newAnimal)
+    createButton();
+
+});
+// on click of an animal button: 
+
+    // display 10 gifs related to that animal
+
+        // play the gif when you click on it, stop the gif when you click it again
+
+    // display the gif's rating
+
+    // ***display the gif's title
+
+    // ***display the gif's tags 
+console.log(animals)
+createButton();
